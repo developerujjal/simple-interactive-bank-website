77c10ce15e25e6d09ad987f7bc5ac861ba315750
Developer Ujjal <ujjalchandra213@gmail.com>
2024-08-18 14:42:20 +0600

    complete a bank interactive website

--- js/deposit.js
+++ js/deposit.js
@@ -4,16 +4,45 @@ document.getElementById("btn-deposit").addEventListener("click", function(){
     // step 2: select the input field element and get the value
 
     let depositElement = document.getElementById("deposit-field");
-    let depositValue = depositElement.value;
-    
+    let depositValueString = depositElement.value;
+    let depositValue = parseFloat(depositValueString);
 
+    if (isNaN(depositValue) === true || depositValue <= 0) {
+        alert("Please enter a valid number!");
+        depositElement.value = "";
+        return;
+    }
+    
     // step 3: select the depost total element and get the value
     // Note: for non-input(input and textarea), we have to use innerText to get the value;
 
     let depositTotalElement = document.getElementById("deposit-total");
-    let depositTotalValue = depositTotalElement.innerText;
+    let depositTotalValueString = depositTotalElement.innerText;
+    let depositTotalValue = parseFloat(depositTotalValueString);
+    
+    // step 4: get the total value by +
+    
+    let currentDepositBalance = depositTotalValue + depositValue;
+    
 
-    // step 4: add the new value of depositTotalValue 
+    depositTotalElement.innerText = currentDepositBalance;
 
-    depositTotalElement.innerText = depositValue;
+
+    // step 5: select the balance field/balance element
+
+    let balanceElement = document.getElementById("balance-field");
+    let balanceElementString = balanceElement.innerText;
+    let balanceValue = parseFloat(balanceElementString);
+
+
+    let currentFinalBalance = balanceValue + depositValue;
+
+    balanceElement.innerText = currentFinalBalance;
+
+    
+
+
+    // last step
+
+    depositElement.value = "";
 })
---
+++ js/withdraw.js
@@ -0,0 +1,51 @@
+/* 
+1. Select the element, where we wanna add event handlyer
+2. Selelct the input field and get the value;
+3. Selelct the element where input field value will added and get the value;
+4. Now add them togather withdrawField value and input field value and also add them into the withdrawTotalElement
+
+5. Select the element where the Balance field, and get the value
+6. make them like: balaceValue - withdrawValue;
+7. Add this variable to the withdrawBalace section
+
+8. for "", we have to setup a withdrawValue string.
+*/
+
+document.getElementById("btn-withdraw").addEventListener("click", function(){
+    let withdrawField = document.getElementById("withdraw-field");
+    let withdrawValueString = withdrawField.value;
+    let withdrawValue = parseFloat(withdrawValueString);
+
+    if(withdrawValue <= 0 || isNaN(withdrawValue) === true){
+        alert("Please provide a Valid Number!");
+        withdrawField.value = "";
+        return;
+    }
+   
+    let withdrawTotalEelement = document.getElementById("withdraw-total");
+    let withdrawTotalValueSting = withdrawTotalEelement.innerText;
+    let withdrawTotalValue = parseFloat(withdrawTotalValueSting);
+
+    let currentWithdrawBalance = withdrawTotalValue + withdrawValue;
+
+    withdrawTotalEelement.innerText = currentWithdrawBalance;
+    
+
+    let balanceElement = document.getElementById("balance-field");
+    let balanceValueString = balanceElement.innerText;
+    let balanceValue = parseFloat(balanceValueString);
+
+    if(balanceValue <= withdrawValue){
+        alert("You don't have enough Money. Please deposit Now!");
+        withdrawTotalEelement.innerText = "00";
+        return;
+    }
+
+    let currectBalanceAfterWithdraw = balanceValue - withdrawValue;
+
+    balanceElement.innerText = currectBalanceAfterWithdraw;
+
+
+    withdrawField.value = '';
+
+})
